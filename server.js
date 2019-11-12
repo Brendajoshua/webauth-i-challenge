4ab6a2d381ee0d34535fe77391662471bf799f21
Brenda Joshua <brendajosh.bj@gmail.com>
2019-11-12 15:48:19 +0300

    configured express-sessions

--- server.js
+++ server.js
@@ -1,10 +1,33 @@
 const express = require('express');
 const helmet = require('helmet');
+const cors = require('cors');
+const sessions = require('express-session');
+const KnexSessionStore = require('connect-session-knex')(sessions);
 
 const userRouter = require('./user/user-router');
+const knexConfig = require('./data/db-config');
 
 const server = express();
 
+const sessionConfig = {
+    name: 'cat',
+    secret: 'Keep it safe',
+    cookie: {
+        httpOnly: true,
+        maxAge: 1000 * 60 * 60,
+        secure: false,
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new KnexSessionStore({
+        knex: knexConfig,
+        createtable: true,
+        clearInterval: 1000 * 60 * 30,
+    }),
+};
+
+server.use(sessions(sessionConfig));
+server.use(cors())
 server.use(helmet());
 server.use(express.json());
 
